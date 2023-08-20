564a750f8863c105d6d317ecc5ff90567e264c38
Kareem Khaled <kareem@Kareems-MacBook-Pro.local>
2023-08-19 22:52:17 -0400

    adding context provider for page selection across site

---
+++ kareem-portfolio/src/contexts/PageContext.js
@@ -0,0 +1,11 @@
+import React, {createContext, useState} from "react";
+const PageContext = createContext();
+
+export const PageProvider = ({ children }) => {
+    const [page, setPage] = useState("");
+    return (
+        <PageContext.Provider value={{ page, setPage }}>
+            {children}
+        </PageContext.Provider>
+    );
+};
