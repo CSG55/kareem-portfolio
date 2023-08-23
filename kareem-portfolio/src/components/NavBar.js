b7c4dde7ff74407f8de6b941609d8dd1708577f3
Kareem Khaled <kareem@Kareems-MacBook-Pro.local>
2023-08-23 13:07:25 -0400

    progress towards building portfolio dashboard - general layout + background img.

---
+++ kareem-portfolio/src/components/NavBar.js
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavBar() {
+    return (
+        <>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    {/*<Navbar.Brand href="#home">Kareem</Navbar.Brand>*/}
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Adventures</Nav.Link>
+                        <Nav.Link href="#features">Projects</Nav.Link>
+                        <Nav.Link href="#pricing">Skills</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    );
+}
+
+export default NavBar;
--- kareem-portfolio/src/components/PortfolioDashboard.js
+++ kareem-portfolio/src/components/PortfolioDashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Col, Row} from "react-bootstrap";
-
+// import profileImage from '/images/kareem-pic-hokkaido.jpg'
 const SummaryBlock = (name, content) =>
     <span>
         <strong> {name}: </strong> <span>{content}</span>
@@ -8,20 +8,22 @@ const SummaryBlock = (name, content) =>
 
 function PortfolioDashboard() {
     return (
-        <div className="portfolio-dashboard">
-            <Row>
-                <img/>
-            </Row>
+        <div className="portfolio-dashboard"
+        >
             <Row>
                 <Col>
-                    My name is Kareem.
-                    A Full Stack Developer.
-                    Skilled, Adventurous.
+                    <div style={{textAlign:'left'}}>
+                        My name is Kareem.<br/>
+                        A Full Stack Developer.<br/>
+                        Skilled, Adventurous.
+                    </div>
                 </Col>
                 <Col>
-                    <button>My Projects</button>
-                    <button>My Skills</button>
-                    <button>My Adventures</button>
+                    <div className={'btn-container'}>
+                        <button>My Projects</button>
+                        <button>My Skills</button>
+                        <button>My Adventures</button>
+                    </div>
                 </Col>
             </Row>
         </div>
