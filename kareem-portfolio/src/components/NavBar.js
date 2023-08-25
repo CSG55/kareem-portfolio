9e299dd438bfafc22ec5203ee654fd57cf7b879f
Kareem Khaled <kareem@Kareems-MacBook-Pro.local>
2023-08-24 20:59:38 -0400

    revamping design for main page - new img, blurb, and button fade-in. mobile+Desktop layouts

--- kareem-portfolio/src/components/NavBar.js
+++ kareem-portfolio/src/components/NavBar.js
@@ -1,21 +1,37 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import {NavDropdown} from "react-bootstrap";
 
 function NavBar() {
     return (
         <>
-            <Navbar data-bs-theme="light">
-                <Container>
-                    {/*<Navbar.Brand href="#home">Kareem</Navbar.Brand>*/}
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Adventures</Nav.Link>
-                        <Nav.Link href="#features">Projects</Nav.Link>
-                        <Nav.Link href="#pricing">Skills</Nav.Link>
-                        <Nav.Link href="#contact">Contact</Nav.Link>
-                    </Nav>
+            {/*<Navbar  bg="light" data-bs-theme="light">*/}
+            {/*    <Container>*/}
+            {/*        <Navbar.Brand href="#home">Kareem Khaled</Navbar.Brand>*/}
+            {/*        <Nav className="ml-auto">*/}
+            {/*            <Nav.Link className="ml-auto" href="#home">Adventures</Nav.Link>*/}
+            {/*            <Nav.Link className="ml-auto" href="#features">Projects</Nav.Link>*/}
+            {/*            <Nav.Link className="ml-auto" href="#pricing">Skills</Nav.Link>*/}
+            {/*            <Nav.Link className="ml-auto" href="#contact">Contact</Nav.Link>*/}
+            {/*        </Nav>*/}
+            {/*    </Container>*/}
+            {/*</Navbar>*/}
+            <Navbar bg="light" data-bs-theme="light" expand="sm" className="bg-body-tertiary">
+                <Container className="bg-body-tertiary">
+                    <Navbar.Brand href="#home">Kareem Khaled</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link className="ml-auto" href="#home">Adventures</Nav.Link>
+                            <Nav.Link className="ml-auto" href="#features">Projects</Nav.Link>
+                            <Nav.Link className="ml-auto" href="#pricing">Skills</Nav.Link>
+                            <Nav.Link className="ml-auto" href="#contact">Contact</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
+
         </>
     );
 }
--- kareem-portfolio/src/components/PortfolioDashboard.js
+++ kareem-portfolio/src/components/PortfolioDashboard.js
@@ -2,48 +2,33 @@ import React from 'react';
 import {Col, Row} from "react-bootstrap";
 import Typewriter from 'typewriter-effect';
 
-const SummaryBlock = (name, content) =>
-    <span>
-        <strong> {name}: </strong> <span>{content}</span>
-    </span>
 
 function PortfolioDashboard() {
+
     return (
         <div className="portfolio-dashboard"
         >
             <Row className={'dashboard-footer-row'}>
-                <Col className={'d-flex justify-content-center'}>
-                    {/*<div style={{textAlign:'left'}}>*/}
-                    {/*    My name is Kareem.<br/>*/}
-                    {/*    A Full Stack Developer.<br/>*/}
-                    {/*    Skilled, Adventurous.*/}
-                    {/*</div>*/}
-                    <Typewriter
-                        options={{
-                            autoStart: true,
-                            loop: false,
-                            delay: 40,
-                            pauseFor: 0,
-                            stop:false,
-                        }}
-                        onInit={(typewriter) => {
-                            typewriter
-                                .typeString(`My name is Kareem.`)
-                                .pauseFor(250)
-                                .typeString(`<br/>A Full Stack Developer.`)
-                                .pauseFor(250)
-                                .typeString(`<br/>Skilled, `)
-                                .pauseFor(500)
-                                .typeString(`Adventurous.`)
-                                .start()
-                        }}
-                    />
-                </Col>
-                <Col className={'d-flex justify-content-center'}>
-                    <div className={'btn-container'}>
-                        <button className='btn btn-primary'>My Projects</button>
-                        <button className='btn btn-primary'>My Skills</button>
-                        <button className='btn btn-primary'>My Adventures</button>
+                <Col className={'justify-content-center'}>
+                    <div className={'typewriter-container'}>
+                        <Typewriter
+                            options={{
+                                autoStart: true,
+                                loop: false,
+                                delay: 40,
+                                pauseFor: 0,
+                                stop:false,
+                            }}
+                            onInit={(typewriter) => {
+                                typewriter
+                                    .typeString(`<span>Developer.</span>`)
+                                    .pauseFor(500)
+                                    .typeString(`<br/><span>Adventurer.</span>`)
+                                    .pauseFor(250)
+                                    .start()
+                            }}
+                        />
+                        <button id="profileBtnFadeIn" className='btn btn-primary'>My Projects</button>
                     </div>
                 </Col>
             </Row>
