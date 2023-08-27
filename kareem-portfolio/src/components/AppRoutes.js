e8525e4e20e726c7ff73b651ca15c0826ea8449b
Kareem Khaled <kareem@Kareems-MacBook-Pro.local>
2023-08-27 16:39:40 -0400

    addressing responsiveness issues on project page

--- kareem-portfolio/src/components/AppRoutes.js
+++ kareem-portfolio/src/components/AppRoutes.js
@@ -4,9 +4,11 @@ import PortfolioDashboard from "./PortfolioDashboard";
 import ProjectDashboard from "./ProjectDashboard";
 export function AppRoutes() {
     return (
-        <Routes>
-            <Route path="/" element={<PortfolioDashboard/>}/>
-            <Route exact path="projects" element={<ProjectDashboard/>}/>
-        </Routes>
+        <div className='app-container'>
+            <Routes>
+                <Route path="/" element={<PortfolioDashboard/>}/>
+                <Route exact path="projects" element={<ProjectDashboard/>}/>
+            </Routes>
+        </div>
     );
 }
