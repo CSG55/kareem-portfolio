402fca72fbb209972b6aafcfee0188057cf4821b
Kareem Khaled <kareem@Kareems-MacBook-Pro.local>
2023-08-27 22:25:43 -0400

    correcting bug with nav routes conflicting with react router

--- kareem-portfolio/src/components/NavBar.js
+++ kareem-portfolio/src/components/NavBar.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {NavDropdown} from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 function NavBar() {
     const navigate = useNavigate();
@@ -11,12 +11,12 @@ function NavBar() {
         <>
             <Navbar bg="light" data-bs-theme="light" expand="sm" className="bg-body-tertiary">
                 <Container className="bg-body-tertiary">
-                    <Navbar.Brand href='/' onClick={() => navigate('/')}>Kareem Khaled</Navbar.Brand>
+                    <Link className={'navbar-brand'} to={'/'}>Kareem Khaled</Link>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
                             <Nav.Link className="ml-auto" href="#home">Adventures</Nav.Link>
-                            <Nav.Link className="ml-auto" href="/projects" onClick={() => navigate('/projects')}>Projects</Nav.Link>
+                            <Link className="ml-auto nav-link" to={'/projects'}>Projects</Link>
                             <Nav.Link className="ml-auto" href="#pricing">Skills</Nav.Link>
                             <Nav.Link className="ml-auto" href="#contact">Contact</Nav.Link>
                         </Nav>
