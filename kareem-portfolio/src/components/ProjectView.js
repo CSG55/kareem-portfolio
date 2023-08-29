acbca93fa03f96b259c4f23bdf6c2eed447f0c55
Kareem Khaled <kareem@Kareems-MacBook-Pro.local>
2023-08-28 21:19:52 -0400

    style adjustments to project background color, navbar font

--- kareem-portfolio/src/components/ProjectView.js
+++ kareem-portfolio/src/components/ProjectView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Row} from "react-bootstrap";
+import {Col, Row} from "react-bootstrap";
 import {SummaryBlock} from "./common";
 
 function ProjectView({title, mission, role, stack, date, description, image}) {
@@ -8,35 +8,35 @@ function ProjectView({title, mission, role, stack, date, description, image}) {
 
     return (
         <div className="project-view">
-            {/*<Row className="top-img">*/}
-            {/*    <img src={image}></img>*/}
-            {/*</Row>*/}
-            <div className="project-summary">
-                {title &&
-                    <SummaryBlock name={'Project'} content={title}/>
-                }
-                {mission &&
-                    <SummaryBlock name={'Mission'} content={mission}/>
-                }
-                {role &&
-                    <SummaryBlock name={'Role'} content={role}/>
-                }
-                {stack &&
-                    <SummaryBlock name={'Stack'} content={stack}/>
-                }
-                {date &&
-                    <SummaryBlock name={'Date'} content={date}/>
-                }
-            </div>
-            {description &&
-                <div className="project-description">
-                    {description}
-                </div>
-            }
-            <div
-            />
-
-
+            <Row>
+                <Col sm={12} lg={6}>
+                    <img src={image}></img>
+                </Col>
+                <Col sm={12} lg={6}>
+                    <div className="project-summary">
+                        {title &&
+                            <SummaryBlock name={'Project'} content={title}/>
+                        }
+                        {mission &&
+                            <SummaryBlock name={'Mission'} content={mission}/>
+                        }
+                        {role &&
+                            <SummaryBlock name={'Role'} content={role}/>
+                        }
+                        {stack &&
+                            <SummaryBlock name={'Stack'} content={stack}/>
+                        }
+                        {date &&
+                            <SummaryBlock name={'Date'} content={date}/>
+                        }
+                    </div>
+                    {description &&
+                        <div className="project-description">
+                            {description}
+                        </div>
+                    }
+                </Col>
+            </Row>
         </div>
     );
 }
