f8526c787d2b6095f7aad701a272ac06e4afd8f6
Kareem Khaled <kareem@Kareems-MacBook-Pro.local>
2023-08-29 00:09:53 -0400

    stylesheet organize

--- kareem-portfolio/src/components/Projects/ProjectDashboard.js
+++ kareem-portfolio/src/components/Projects/ProjectDashboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import InteractivePanel from '../common/InteractivePanel';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCoffee, faQuoteLeft, faQuoteRight} from "@fortawesome/free-solid-svg-icons";
-import {projectData} from "../common";
+import {projectData} from "../../projectData";
 
 const ProjectDashboard = () => {
 
