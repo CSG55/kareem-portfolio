82bec55149be92b151aaebf72538989260692985
Kareem Khaled <kareem@Kareems-MacBook-Pro.local>
2023-08-30 15:22:25 -0400

    solving bad img zoom with react image gallery, using fullscreen

--- kareem-portfolio/src/components/Projects/ProjectView.js
+++ kareem-portfolio/src/components/Projects/ProjectView.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {Col, Row} from "react-bootstrap";
 import {SummaryBlock} from "../common";
+import ImageGallery from "react-image-gallery";
+
 
 function ProjectView({title, mission, role, stack, date, description, image}) {
-    console.log(description)
-    console.log(typeof (description))
+    const imageGalleryRef = useRef(null);
+
+    const onImageClickHandler = () => {
+        console.log(imageGalleryRef.current)
+        if (imageGalleryRef.current.state.isFullscreen){
+            imageGalleryRef.current.exitFullScreen();
+        } else {
+            imageGalleryRef.current.fullScreen();
+
+        }
+    };
+
+    const images = [
+        {
+            original: image,
+            originalAlt:`${title} project photo`,
+        },
+    ];
 
     return (
         <div className="project-view">
             <Row>
-                <Col className={'project-img'} sm={12} md={7} lg={7}>
-                    <img src={image} alt={`${title} project photo`}></img>
+                <Col className={'project-img'} sm={12} md={8} lg={8}>
+                    <ImageGallery
+                        items={images}
+                        showPlayButton={false}
+                        useBrowserFullscreen={false}
+                        ref={imageGalleryRef}
+                        onClick={onImageClickHandler}
+                    />
                 </Col>
-                <Col sm={12} lg={5}>
+                <Col sm={12} md={4} lg={4}>
                     <div className="project-summary">
                         {title &&
                             <SummaryBlock name={'Project'} content={title}/>
