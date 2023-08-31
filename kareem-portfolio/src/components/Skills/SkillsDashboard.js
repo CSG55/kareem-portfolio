69020cad918a61f57dfd4dc3d3387c8ff13b6d45
Kareem Khaled <kareem@Kareems-MacBook-Pro.local>
2023-08-30 21:48:38 -0400

    more responsive structure for skills page

--- kareem-portfolio/src/components/Skills/SkillsDashboard.js
+++ kareem-portfolio/src/components/Skills/SkillsDashboard.js
@@ -4,7 +4,7 @@ import {SummaryBlock} from "../common";
 import ImageGallery from "react-image-gallery";
 import { useNavigate} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faArrowLeft, faQuoteLeft} from "@fortawesome/free-solid-svg-icons";
+import {faArrowLeft, faLaptopCode, faQuoteLeft} from "@fortawesome/free-solid-svg-icons";
 
 
 function SkillsDashboard() {
@@ -12,20 +12,21 @@ function SkillsDashboard() {
     return (
         <div className="skills-view">
             <Row>
-                <Col xs={12} sm={5} md={5} lg={5}>
+                <Col xs={12} sm={5} md={4} lg={4} xl={3}>
                     <img className={'headshot-img'} src={'/images/kareem-headshot-crop.png'} alt={'kareem head shot'}></img>
                 </Col>
-                <Col className={'short-blurb-container'} xs={12} sm={7} md={7} lg={7}>
+                <Col className={'short-blurb-container'} xs={12} sm={7} md={8} lg={8} xl={9}>
                     <div className="short-blurb">
                         Hello my name is Kareem. Professor: News! Bad! We're bankrupt. Oh, I borrowed money to invest in Bitcoin. Then the price crashed, and I sold at a huge loss. We're ruined! Oh, sorry. I didn't realize you were watching TV. Morbo: The tragic impact destroyed the planet Klarg, along with its 50 billion inhabitants. Linda: [Laughs] In other news, Morbo, guess what's back? Morbo: The planet Klarg? Linda: Nope! The price of Bitcoin!
                     </div>
                     <button className={'btn btn-primary'}>Contact me</button>
                 </Col>
             </Row>
-            <Row className={'skills-header'}>
-                <Col sm={6} md={6} lg={6}>
-                    <Row>
-                        <h4 className={'skills-heading'}>Tech</h4>
+            <Row className={'skills-header-container'}>
+                <Col className={'skills-column'} sm={6} md={6} lg={6}>
+                    <div className={"skills-header-row"}>
+                        <h4>Tech</h4>
+                        <FontAwesomeIcon icon={faLaptopCode}/>
                         <div className={'skills-list-container'}>
                             <ul className={'skills-list'}>
                                 <li>ReactJS</li>
@@ -38,10 +39,25 @@ function SkillsDashboard() {
                                 <li>API, Websockets</li>
                             </ul>
                         </div>
-                    </Row>
+                    </div>
                 </Col>
-                <Col sm={6} md={6} lg={6}>
-                    <h4 className={'skills-heading'}>Skills</h4>
+                <Col className={'skills-column'} sm={6} md={6} lg={6}>
+                    <div className={"skills-header-row"}>
+                        <h4>Skills</h4>
+                        <FontAwesomeIcon icon={faLaptopCode}/>
+                        <div className={'skills-list-container'}>
+                            <ul className={'skills-list'}>
+                                <li>ReactJS</li>
+                                <li>Python (Flask/Django)</li>
+                                <li>NodeJS</li>
+                            </ul>
+                            <ul className={'skills-list'}>
+                                <li>AWS</li>
+                                <li>SQL/NoSQL</li>
+                                <li>API, Websockets</li>
+                            </ul>
+                        </div>
+                    </div>
                 </Col>
             </Row>
         </div>
