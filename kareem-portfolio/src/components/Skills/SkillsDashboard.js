337f963c979bb23fbd48ea8220c42e5339801e37
Kareem Khaled <kareem@Kareems-MacBook-Pro.local>
2023-08-30 22:25:54 -0400

    more responsive structure for skills page

--- kareem-portfolio/src/components/Skills/SkillsDashboard.js
+++ kareem-portfolio/src/components/Skills/SkillsDashboard.js
@@ -4,7 +4,13 @@ import {SummaryBlock} from "../common";
 import ImageGallery from "react-image-gallery";
 import { useNavigate} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faArrowLeft, faLaptopCode, faQuoteLeft} from "@fortawesome/free-solid-svg-icons";
+import {
+    faArrowLeft, faHandshake,
+    faLaptopCode,
+    faMicroscope,
+    faPersonChalkboard, faQuestion,
+    faQuoteLeft
+} from "@fortawesome/free-solid-svg-icons";
 
 
 function SkillsDashboard() {
@@ -12,10 +18,10 @@ function SkillsDashboard() {
     return (
         <div className="skills-view">
             <Row>
-                <Col xs={12} sm={5} md={4} lg={4} xl={3}>
+                <Col xs={12} sm={5} md={5} lg={4} xl={3}>
                     <img className={'headshot-img'} src={'/images/kareem-headshot-crop.png'} alt={'kareem head shot'}></img>
                 </Col>
-                <Col className={'short-blurb-container'} xs={12} sm={7} md={8} lg={8} xl={9}>
+                <Col className={'short-blurb-container'} xs={12} sm={7} md={7} lg={8} xl={9}>
                     <div className="short-blurb">
                         Hello my name is Kareem. Professor: News! Bad! We're bankrupt. Oh, I borrowed money to invest in Bitcoin. Then the price crashed, and I sold at a huge loss. We're ruined! Oh, sorry. I didn't realize you were watching TV. Morbo: The tragic impact destroyed the planet Klarg, along with its 50 billion inhabitants. Linda: [Laughs] In other news, Morbo, guess what's back? Morbo: The planet Klarg? Linda: Nope! The price of Bitcoin!
                     </div>
@@ -26,7 +32,7 @@ function SkillsDashboard() {
                 <Col className={'skills-column'} sm={6} md={6} lg={6}>
                     <div className={"skills-header-row"}>
                         <h4>Tech</h4>
-                        <FontAwesomeIcon icon={faLaptopCode}/>
+                        <FontAwesomeIcon icon={faLaptopCode} size={"lg"}/>
                         <div className={'skills-list-container'}>
                             <ul className={'skills-list'}>
                                 <li>ReactJS</li>
@@ -37,6 +43,7 @@ function SkillsDashboard() {
                                 <li>AWS</li>
                                 <li>SQL/NoSQL</li>
                                 <li>API, Websockets</li>
+                                {/*<li>Testing</li>*/}
                             </ul>
                         </div>
                     </div>
@@ -44,18 +51,15 @@ function SkillsDashboard() {
                 <Col className={'skills-column'} sm={6} md={6} lg={6}>
                     <div className={"skills-header-row"}>
                         <h4>Skills</h4>
-                        <FontAwesomeIcon icon={faLaptopCode}/>
                         <div className={'skills-list-container'}>
-                            <ul className={'skills-list'}>
-                                <li>ReactJS</li>
-                                <li>Python (Flask/Django)</li>
-                                <li>NodeJS</li>
-                            </ul>
-                            <ul className={'skills-list'}>
-                                <li>AWS</li>
-                                <li>SQL/NoSQL</li>
-                                <li>API, Websockets</li>
-                            </ul>
+                            <div className={'soft-skill-col'}>
+                                <div><FontAwesomeIcon icon={faMicroscope} size={"lg"}/> Observability </div>
+                                <div><FontAwesomeIcon icon={faQuestion} size={"lg"}/> Ambiguity </div>
+                            </div>
+                            <div className={'soft-skill-col'}>
+                                <div><FontAwesomeIcon icon={faHandshake} size={"lg"}/> Leadership </div>
+                                <div><FontAwesomeIcon icon={faPersonChalkboard} size={"lg"}/> Presentations </div>
+                            </div>
                         </div>
                     </div>
                 </Col>
